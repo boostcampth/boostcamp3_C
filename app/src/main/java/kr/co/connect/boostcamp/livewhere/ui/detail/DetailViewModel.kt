7d681dcf3a9795cae1720680a2b3144f9ad51212
최준영 <chad1994@skuniv.ac.kr>
2019-01-29 13:53:44 +0900

    #5 detail페이지 activity,fragment,viewmodel 구성

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/detail/DetailViewModel.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/detail/DetailViewModel.kt
@@ -1,2 +1,10 @@
 package kr.co.connect.boostcamp.livewhere.ui.detail
 
+import kr.co.connect.boostcamp.livewhere.repository.DetailRepository
+import kr.co.connect.boostcamp.livewhere.ui.BaseViewModel
+
+class DetailViewModel(private val detailRepository: DetailRepository) : BaseViewModel(){
+
+    init {}
+
+}
