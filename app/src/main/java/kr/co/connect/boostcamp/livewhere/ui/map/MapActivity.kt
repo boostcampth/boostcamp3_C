e6fea3078dcecd7b461ea5e3ad454d5509d8c119
hakzzang <mbh930312@naver.com>
2019-01-29 13:18:26 +0900

    #6 map 관련 repositroy 인터페이스 및 클래스 구조

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/map/MapActivity.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/map/MapActivity.kt
@@ -1,19 +1,52 @@
 package kr.co.connect.boostcamp.livewhere.ui.map
 
+import android.graphics.PointF
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.skt.Tmap.*
+import kotlinx.android.synthetic.main.activity_map.*
 import kr.co.connect.boostcamp.livewhere.R
 import kr.co.connect.boostcamp.livewhere.databinding.ActivityMapBinding
-import kr.co.connect.boostcamp.livewhere.util.MapUtilIImp
+import kr.co.connect.boostcamp.livewhere.util.MapHelper
+import kr.co.connect.boostcamp.livewhere.util.MapHelperImpl
+import kr.co.connect.boostcamp.livewhere.util.MapUtilImpl
+import java.util.ArrayList
 import javax.inject.Inject
 
-class MapActivity : AppCompatActivity(){
-    @Inject
-    lateinit var mapUtilImp:MapUtilIImp
+class MapActivity : AppCompatActivity() {
+    lateinit var mapUtilImpl: MapUtilImpl
+    lateinit var mapHelper: MapHelperImpl
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val mapBindingUtil:ActivityMapBinding=DataBindingUtil.setContentView(this, R.layout.activity_map)
+        setContentView(R.layout.activity_map)
+        /*val tMapView = TMapView(this)
+        tMapView.setSKTMapApiKey("aefcae11-4e30-4cc2-b2c8-790a9cde8875")
+        ll_map_view.addView(tMapView)
+        tMapView.setOnClickListenerCallBack(object : TMapView.OnClickListenerCallback {
+            override fun onPressEvent(
+                p0: ArrayList<TMapMarkerItem>?,
+                p1: ArrayList<TMapPOIItem>?,
+                p2: TMapPoint?,
+                p3: PointF?
+            ): Boolean {
+                val tampItem = TMapMarkerItem()
+                tampItem.setPosition(p2!!.latitude.toFloat(), p2.longitude.toFloat())
+                Log.d("pos",tampItem.positionX.toString()+", "+tampItem.positionY)
+                return true
+            }
+
+            override fun onPressUpEvent(
+                p0: ArrayList<TMapMarkerItem>?,
+                p1: ArrayList<TMapPOIItem>?,
+                p2: TMapPoint?,
+                p3: PointF?
+            ): Boolean {
+                return true
+            }
+        })*/
     }
 }
