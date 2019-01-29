6ae98b01ef4858e25f169950602e28c6587ccfe8
최준영 <chad1994@skuniv.ac.kr>
2019-01-29 12:35:53 +0900

    #5 detailRepository에 detail페이지 인터페이스 기능 정의 - 상세정보 리스트 호출

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/repository/DetailRepository.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/repository/DetailRepository.kt
@@ -1,2 +1,18 @@
 package kr.co.connect.boostcamp.livewhere.repository
 
+import io.reactivex.Single
+import kr.co.connect.boostcamp.livewhere.api.Api
+import retrofit2.Response
+
+interface DetailRepository {
+
+    fun getDetail(address:String): Single<Response<List<Any>>>
+
+}
+
+class DetailRepositoryImpl(private val api : Api) : DetailRepository{
+    override fun getDetail(address: String): Single<Response<List<Any>>> {
+//        return api.getDetail(address)
+        return api.getHouseDetail(address)
+    }
+}
