ac4fd998fd08102cc59badb077dc961f56ca91bc
최준영 <chad1994@skuniv.ac.kr>
2019-01-29 13:13:41 +0900

    #5 Glide 라이브러리 추가

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/LiveApplication.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/LiveApplication.kt
@@ -1,13 +1,28 @@
 package kr.co.connect.boostcamp.livewhere
 
 import android.app.Application
+import com.bumptech.glide.Glide
+import com.bumptech.glide.RequestManager
 import kr.co.connect.boostcamp.livewhere.di.appModules
 import org.koin.android.ext.android.startKoin
 
 class LiveApplication : Application(){
 
+    lateinit var glide : Glide
+
     override fun onCreate() {
         super.onCreate()
         startKoin(applicationContext, appModules)
+        glide = Glide.get(this)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        glide.clearMemory()
+    }
+
+    override fun onTrimMemory(level: Int) {
+        super.onTrimMemory(level)
+        glide.trimMemory(level)
     }
 }
