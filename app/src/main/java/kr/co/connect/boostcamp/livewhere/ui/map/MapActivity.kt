1a566f7251d91708f848b1cc2a3b5ae951893a58
hakzzang <mbh930312@naver.com>
2019-01-29 13:45:38 +0900

    #6 api키 제거, retrofit gradle version 분리

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/map/MapActivity.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/map/MapActivity.kt
@@ -13,30 +13,5 @@ class MapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
-        /*val tMapView = TMapView(this)
-        tMapView.setSKTMapApiKey("aefcae11-4e30-4cc2-b2c8-790a9cde8875")
-        ll_map_view.addView(tMapView)
-        tMapView.setOnClickListenerCallBack(object : TMapView.OnClickListenerCallback {
-            override fun onPressEvent(
-                p0: ArrayList<TMapMarkerItem>?,
-                p1: ArrayList<TMapPOIItem>?,
-                p2: TMapPoint?,
-                p3: PointF?
-            ): Boolean {
-                val tampItem = TMapMarkerItem()
-                tampItem.setPosition(p2!!.latitude.toFloat(), p2.longitude.toFloat())
-                Log.d("pos",tampItem.positionX.toString()+", "+tampItem.positionY)
-                return true
-            }
-
-            override fun onPressUpEvent(
-                p0: ArrayList<TMapMarkerItem>?,
-                p1: ArrayList<TMapPOIItem>?,
-                p2: TMapPoint?,
-                p3: PointF?
-            ): Boolean {
-                return true
-            }
-        })*/
     }
 }
