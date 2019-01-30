dbc69ee5e2156f75b689e1bfaac55dad75bb183e
weedrice <supp0rtyoo@gmail.com>
2019-01-30 18:12:42 +0900

    init binding, viewmodel in HomeActivity

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/main/HomeActivity.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/main/HomeActivity.kt
@@ -2,11 +2,17 @@ package kr.co.connect.boostcamp.livewhere.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import kr.co.connect.boostcamp.livewhere.R
+import kr.co.connect.boostcamp.livewhere.databinding.ActivityHomeBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeBinding
+    private val homeViewModel: HomeActivityViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home)
     }
 }
