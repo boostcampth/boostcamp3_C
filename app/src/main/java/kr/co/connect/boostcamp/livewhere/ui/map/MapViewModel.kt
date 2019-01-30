ab145345593e509c69277477913476bd62d09ac5
hakzzang <mbh930312@naver.com>
2019-01-30 11:04:31 +0900

    #9 koin 관련 inject 및 MapViewModel, BindingAdapter 작업

---
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/map/MapViewModel.kt
@@ -0,0 +1,9 @@
+package kr.co.connect.boostcamp.livewhere.ui.map
+
+import androidx.lifecycle.ViewModel
+import kr.co.connect.boostcamp.livewhere.util.MapHelperImpl
+import kr.co.connect.boostcamp.livewhere.util.MapUtilImpl
+
+class MapViewModel(val mapHelperImpl: MapHelperImpl, val mapUtilImpl: MapUtilImpl) : ViewModel(){
+
+}
