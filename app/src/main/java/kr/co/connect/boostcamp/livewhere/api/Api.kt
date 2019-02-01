2a065e50104caa28fae53f0c066a1849bae05784
hakzzang <mbh930312@naver.com>
2019-02-01 17:47:53 +0900

    #46 get api 관련 conflict 변경

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/api/Api.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/api/Api.kt
@@ -2,10 +2,16 @@ package kr.co.connect.boostcamp.livewhere.api
 
 import io.reactivex.Single
 import kr.co.connect.boostcamp.livewhere.model.HouseResponse
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface Api{
+    @GET("house/search/infos")
+    fun getDetail(
+        @Query("address")address:String
+    ):Single<Response<List<Any>>>
+    
     @GET("house/search/infos")
     fun getHouseDetail(
         @Query("address")address:String
--- app/src/main/java/kr/co/connect/boostcamp/livewhere/repository/DetailRepository.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/repository/DetailRepository.kt
@@ -5,13 +5,12 @@ import kr.co.connect.boostcamp.livewhere.api.Api
 import retrofit2.Response
 
 interface DetailRepository {
-
     fun getDetail(address:String): Single<Response<List<Any>>>
 
 }
 
 class DetailRepositoryImpl(private val api : Api) : DetailRepository{
     override fun getDetail(address: String): Single<Response<List<Any>>> {
-        return api.getHouseDetail(address)
+        return api.getDetail(address)
     }
 }
