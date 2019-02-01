51403439b98d19e80a9515cfd7a68c0ba96674aa
최준영 <chad1994@skuniv.ac.kr>
2019-02-01 15:53:52 +0900

    #44 과거 거래 내역 list데이터 추가

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/detail/DetailViewModel.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/detail/DetailViewModel.kt
@@ -3,21 +3,26 @@ package kr.co.connect.boostcamp.livewhere.ui.detail
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import kr.co.connect.boostcamp.livewhere.model.HouseAvgPrice
+import kr.co.connect.boostcamp.livewhere.model.PastTransaction
 import kr.co.connect.boostcamp.livewhere.repository.DetailRepository
 import kr.co.connect.boostcamp.livewhere.ui.BaseViewModel
 
-class DetailViewModel(private val detailRepository: DetailRepository) : BaseViewModel(){
+class DetailViewModel(private val detailRepository: DetailRepository) : BaseViewModel() {
 
     private val _coordinate = MutableLiveData<String>()
-    val coordinate : LiveData<String>
+    val coordinate: LiveData<String>
         get() = _coordinate
 
     private val _listHouseAvgPrice = MutableLiveData<ArrayList<HouseAvgPrice>>()
-    val listHouseAvgPrice : LiveData<ArrayList<HouseAvgPrice>>
+    val listHouseAvgPrice: LiveData<ArrayList<HouseAvgPrice>>
         get() = _listHouseAvgPrice
 
+    private val _pastTransaction = MutableLiveData<ArrayList<PastTransaction>>()
+    val pastTransaction: LiveData<ArrayList<PastTransaction>>
+        get() = _pastTransaction
+
     init {
-        _coordinate.postValue("37.515238,127.032937")
+        // 필요 변수 초기화
     }
 
 }
