8a922843d95a40fce0bae467ff858f64f50b33dc
hakzzang <mbh930312@naver.com>
2019-02-01 17:37:46 +0900

    #46 HttpResult enum 및 MapUtil 클래스 변경

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/util/MapUtil.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/util/MapUtil.kt
@@ -1,7 +1,16 @@
 package kr.co.connect.boostcamp.livewhere.util
 
-interface MapUtil{
+import android.content.Context
+import android.graphics.Bitmap
+import com.skt.Tmap.TMapMarkerItem
+import com.skt.Tmap.TMapPoint
+
+interface MapUtil {
     fun filterMap(category: CharCategory)
 
     fun findMyLocation()
+
+    fun makeMarker(point: TMapPoint, markerBitmap: Bitmap): TMapMarkerItem
+
+    fun makeOverlay(context: Context, tMapPoint: TMapPoint, title: String, subTitle: String) : MarkerOverlay
 }
