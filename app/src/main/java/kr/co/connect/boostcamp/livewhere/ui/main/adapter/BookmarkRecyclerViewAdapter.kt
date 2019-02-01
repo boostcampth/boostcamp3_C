f9706a18125eb37a84416e41c587a83f942efb5c
weedrice <supp0rtyoo@gmail.com>
2019-02-01 16:05:50 +0900

    #37 Replace Hardcoded String Value to Declared in String.xml, Add Image process in Adapter by using Glide.

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/main/adapter/BookmarkRecyclerViewAdapter.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/main/adapter/BookmarkRecyclerViewAdapter.kt
@@ -11,10 +11,8 @@ import com.bumptech.glide.Glide
 import kr.co.connect.boostcamp.livewhere.R
 import kr.co.connect.boostcamp.livewhere.model.Bookmark
 
-class BookmarkRecyclerViewAdapter(private val context: Context, private val list: ArrayList<Bookmark>)
-    : RecyclerView.Adapter<BookmarkRecyclerViewAdapter.ViewHolder>() {
-    private val NO_PEE : String = "전세"
-    private val YES_PEE : String = "월세"
+class BookmarkRecyclerViewAdapter(private val context: Context, private val list: ArrayList<Bookmark>) :
+    RecyclerView.Adapter<BookmarkRecyclerViewAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookmarkRecyclerViewAdapter.ViewHolder {
         val view = LayoutInflater
@@ -26,25 +24,30 @@ class BookmarkRecyclerViewAdapter(private val context: Context, private val list
     override fun getItemCount() = list.size
 
     override fun onBindViewHolder(holder: BookmarkRecyclerViewAdapter.ViewHolder, position: Int) {
-        holder.bind(list[position])
+        holder.bind(context, list[position])
     }
 
     inner class ViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView!!) {
         val iv_building = itemView?.findViewById<ImageView>(R.id.iv_bookmark_image)
-        val tv_building_contents  = itemView?.findViewById<TextView>(R.id.tv_bookmark_building_contents)
-        val tv_location_contents  = itemView?.findViewById<TextView>(R.id.tv_bookmark_location_contents)
-        val tv_pay_title  = itemView?.findViewById<TextView>(R.id.tv_bookmark_pay_title)
-        val tv_pay_contents  = itemView?.findViewById<TextView>(R.id.tv_bookmark_pay_contents)
+        val tv_building_contents = itemView?.findViewById<TextView>(R.id.tv_bookmark_building_contents)
+        val tv_location_contents = itemView?.findViewById<TextView>(R.id.tv_bookmark_location_contents)
+        val tv_pay_contents = itemView?.findViewById<TextView>(R.id.tv_bookmark_pay_contents)
 
-        fun bind(item: Bookmark) {
+        fun bind(context: Context, item: Bookmark) {
             //image 추후 추가
+            if (iv_building != null)
+                Glide.with(context)
+                    .load(item.imgUrl)
+                    .into(iv_building)
+            /*
+            type when default images are added
+            else
+             */
             tv_building_contents?.text = item.buildingName
             tv_location_contents?.text = item.address
-            if(item.isRent) {
-                tv_pay_title?.text = YES_PEE
+            if (item.isRent) {
                 tv_pay_contents?.text = item.pee.toString()
             } else {
-                tv_pay_title?.text = NO_PEE
                 tv_pay_contents?.text = item.deposit.toString()
             }
         }
