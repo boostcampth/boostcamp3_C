63e2832256b6cb37dadd494c1ee7e842cec0602f
hakzzang <mbh930312@naver.com>
2019-02-03 21:45:53 +0900

    #63 MapActivity 및 ViewModel, BindingUtil 작업

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/map/MapActivity.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/map/MapActivity.kt
@@ -3,25 +3,71 @@ package kr.co.connect.boostcamp.livewhere.ui.map
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.naver.maps.map.util.FusedLocationSource
 import kr.co.connect.boostcamp.livewhere.BR
 import kr.co.connect.boostcamp.livewhere.R
 import kr.co.connect.boostcamp.livewhere.databinding.ActivityMapBinding
+import kr.co.connect.boostcamp.livewhere.util.MapUtilImpl.Companion.LOCATION_PERMISSION_REQUEST_CODE
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
 class MapActivity : AppCompatActivity() {
     private val mapViewModel: MapViewModel by viewModel()
+    private lateinit var activityMapBinding: ActivityMapBinding
+    private lateinit var locationSource : FusedLocationSource
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        val activityMapBinding: ActivityMapBinding = DataBindingUtil.setContentView(this, R.layout.activity_map)
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
+        activityMapBinding = DataBindingUtil.setContentView(this, R.layout.activity_map)
         activityMapBinding.setLifecycleOwner(this)
         activityMapBinding.setVariable(BR.mapViewModel, mapViewModel)
         activityMapBinding.setVariable(BR.mlFloatBtn, activityMapBinding.mlFloatBtn)
         activityMapBinding.setVariable(BR.mlBackdrop, activityMapBinding.mlBackdrop)
+        activityMapBinding.setVariable(BR.locationSource, locationSource)
+        activityMapBinding.mvMainNaver.onCreate(savedInstanceState)
+        activityMapBinding.mvMainNaver.getMapAsync(mapViewModel)
     }
+
+    override fun onStart() {
+        super.onStart()
+        activityMapBinding.mvMainNaver.onStart()//naverMap객체는 라이프사이클에 종속되어야 합니다.
+    }
+
+    override fun onResume() {
+        super.onResume()
+        activityMapBinding.mvMainNaver.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        activityMapBinding.mvMainNaver.onPause()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        activityMapBinding.mvMainNaver.onSaveInstanceState(outState)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        activityMapBinding.mvMainNaver.onStop()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        activityMapBinding.mvMainNaver.onLowMemory()
+    }
+
+    //자신의 위치 관련한 permission 함수
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+
 }
--- app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/map/MapBindingUtil.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/map/MapBindingUtil.kt
@@ -1,41 +1,108 @@
 package kr.co.connect.boostcamp.livewhere.ui.map
 
-import android.graphics.BitmapFactory
-import android.widget.LinearLayout
+import android.util.Log
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.LiveData
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.skt.Tmap.TMapPoint
-import com.skt.Tmap.TMapView
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.LocationSource
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.MapView
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.overlay.InfoWindow
+import com.naver.maps.map.overlay.Marker
+import com.naver.maps.map.widget.LocationButtonView
+import com.naver.maps.map.widget.ZoomControlView
 import kr.co.connect.boostcamp.livewhere.BuildConfig
 import kr.co.connect.boostcamp.livewhere.R
+import kr.co.connect.boostcamp.livewhere.model.MarkerInfo
+import kr.co.connect.boostcamp.livewhere.util.StatusCode
 
-@BindingAdapter(value = ["isCreateTMap"])
-fun LinearLayout.initMap(isCreateMap: Boolean) {
-    if(isCreateMap){
-        val tMapView = TMapView(this.context)
 
-        tMapView.setSKTMapApiKey(BuildConfig.TmapApiKey)
-        this.addView(tMapView)
+//맵 초기화 관련 onMakeNaverMapData
+@BindingAdapter(value = ["onMakeNaverMapData", "bindViewModel"])
+fun MapView.onMakeNaverMap(mapStatusLiveData: LiveData<NaverMap>, mapViewModel: MapViewModel) {
+    Log.d("naverStatus", "make")
+    if (mapStatusLiveData.value != null) {
+        val naverMap: NaverMap = mapStatusLiveData.value!!
+        naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_BUILDING, true)//building
+        naverMap.setLayerGroupEnabled(NaverMap.LAYER_GROUP_TRANSIT, true)//대중교통
+        naverMap.uiSettings.isCompassEnabled = true // 나침반
+        naverMap.uiSettings.isLogoClickEnabled = false//로고 클릭 이벤트
+        naverMap.locationTrackingMode = LocationTrackingMode.Face//위치추적모드
+        naverMap.locationTrackingMode = LocationTrackingMode.Follow
+        naverMap.uiSettings.isLocationButtonEnabled = false//위치찾기 버튼 이벤트
+        naverMap.uiSettings.isScaleBarEnabled = false
+        naverMap.isIndoorEnabled = true//실내모드
+        naverMap.uiSettings.isIndoorLevelPickerEnabled = true
+        naverMap.uiSettings.isZoomControlEnabled = false//줌 버튼 이벤트
+        naverMap.onMapLongClickListener = mapViewModel //맵클릭
+        naverMap.onMapClickListener = mapViewModel //맵롱클릭
     }
 }
 
+@BindingAdapter(value = ["onDrawMarker"])
+fun MapView.OnDrawMarker(markerInfoLiveData: LiveData<MarkerInfo>) {
+    val markerInfo = markerInfoLiveData.value
+    if (markerInfo != null) {
+        val latLang = markerInfo.latLng
+        val houseList = markerInfo.houseList
+        val statusCode = markerInfo.statusCode
+        getMapAsync { naverMap ->
+            val infoWindow = InfoWindow()
+            val marker = Marker()
+            var streetImgUrl=""
+            var title =""
+            if (statusCode == StatusCode.RESULT_200) {
+                streetImgUrl = String.format(BuildConfig.BaseGoogleUrl, latLang.latitude, latLang.longitude, BuildConfig.GoogleApiKey)
+                title = houseList[0].rentCase + ":" + houseList[0].deposite + "/" + houseList[0].fee
+                marker.tag = houseList[0].name
+            } else if (statusCode == StatusCode.RESULT_204) {
+                streetImgUrl = ""
+                title = context.resources.getString(R.string.title_detail_history_empty)
+                marker.tag =""
+            }
+            infoWindow.position = LatLng(latLang.latitude, latLang.longitude)
+            infoWindow.adapter = MapMarkerAdapter(context, parent, streetImgUrl, title)
+            infoWindow.setOnClickListener { _ ->
+                infoWindow.close()
+                true
+            }
 
-@BindingAdapter(value=["setOnLongClickListenerCallback"])
-fun LinearLayout.setOnLongClickListenerCallback(mapViewModel:MapViewModel){
-    val tMapView = getChildAt(0) as TMapView
-    val context = tMapView.context
-    tMapView.setOnLongClickListenerCallback(mapViewModel)
+            if (tag != null) {
+                val tempMarker = tag as Marker
+                tempMarker.map = null
+                tempMarker.infoWindow?.close()
+            }
+            marker.position = LatLng(latLang.latitude, latLang.longitude)
+            marker.setOnClickListener { overlay ->
+                infoWindow.open(marker)
+                true
+            }
+
+            marker.map = naverMap
+            tag = marker
+        }
+    }
 }
 
-@BindingAdapter(value=["onDrawMarker","onPointLiveData"])
-fun LinearLayout.onDrawMarker(mapViewModel: MapViewModel, pointLiveData: LiveData<TMapPoint>){
-    val tMapView = getChildAt(0) as TMapView
-    val markerBitmap = BitmapFactory.decodeResource(this.context.resources, R.drawable.ic_map_marker_24dp)
-    if(pointLiveData.value!=null){
-        val tMapMarkerItem = mapViewModel.mapUtilImpl.makeMarker(pointLiveData.value!!, markerBitmap)
-        tMapView.addMarkerItem("marker", tMapMarkerItem)
+//zoom button 관련 onMakeNaverMapData
+@BindingAdapter(value = ["onMakeNaverMapData"])
+fun ZoomControlView.setOnClick(mapStatusLiveData: LiveData<NaverMap>) {
+    map = mapStatusLiveData.value
+    if (map != null) {
+        map!!.uiSettings.isZoomControlEnabled = true
+    }
+}
+
+//위치 찾기 관련 onMakeNaverMapData
+@BindingAdapter(value = ["onMakeNaverMapData", "onLocationSource"])
+fun LocationButtonView.setOnClick(mapStatusLiveData: LiveData<NaverMap>, locationSource: LocationSource) {
+    map = mapStatusLiveData.value
+    if (map != null) {
+        map!!.locationSource = locationSource
+        map!!.locationSource = mapStatusLiveData.value!!.locationSource
     }
 }
 
