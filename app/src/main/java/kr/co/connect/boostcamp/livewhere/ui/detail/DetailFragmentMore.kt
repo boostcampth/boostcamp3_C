6e93910d97acf073d4270a865cbe05d5a7603d36
최준영 <chad1994@skuniv.ac.kr>
2019-02-03 18:47:53 +0900

    #52 과거거래내역 더보기 페이지 fragment구현

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/detail/DetailFragmentMore.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/detail/DetailFragmentMore.kt
@@ -1,2 +1,44 @@
 package kr.co.connect.boostcamp.livewhere.ui.detail
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import kr.co.connect.boostcamp.livewhere.databinding.FragmentDetailPastTransactionMoreBinding
+import kr.co.connect.boostcamp.livewhere.ui.detail.adapter.DetailRvAdapter
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+
+class DetailFragmentMore : Fragment() {
+
+    companion object {
+        fun newInstance(): DetailFragmentMore {
+            val args = Bundle()
+            val fragment = DetailFragmentMore()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    private val viewModel: DetailViewModel by sharedViewModel()
+    private lateinit var binding: FragmentDetailPastTransactionMoreBinding
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentDetailPastTransactionMoreBinding.inflate(inflater,container,false).apply {
+            viewModel = this@DetailFragmentMore.viewModel
+            setLifecycleOwner(this@DetailFragmentMore)
+        }
+
+        binding.pastTransactionMoreRv.apply{
+            layoutManager = LinearLayoutManager(context)
+            adapter = DetailRvAdapter(this@DetailFragmentMore)
+        }
+
+        return binding.root
+    }
+
+
+
+}
