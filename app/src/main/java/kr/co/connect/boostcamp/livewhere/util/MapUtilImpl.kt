dde5fcee442d144ca53a4eb03203bada72e13941
hakzzang <mbh930312@naver.com>
2019-02-03 21:44:36 +0900

    #63 liveApplication 변경 및 MapUtil 구조 변경

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/util/MapUtilImpl.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/util/MapUtilImpl.kt
@@ -1,37 +1,11 @@
 package kr.co.connect.boostcamp.livewhere.util
 
-import android.content.Context
-import android.graphics.Bitmap
-import com.skt.Tmap.TMapMarkerItem
-import com.skt.Tmap.TMapPoint
+interface MapUtil {
+
+}
 
 class MapUtilImpl : MapUtil {
-    override fun makeOverlay(context: Context, tMapPoint: TMapPoint, title: String, subTitle: String): MarkerOverlay {
-        val strID = "TMapMarkerItem2"
-        val markerOverlay = MarkerOverlay(context, "custom", "marker")
-        markerOverlay.id = strID
-        markerOverlay.tMapPoint = TMapPoint(tMapPoint.latitude, tMapPoint.longitude)
-        markerOverlay.balloonView?.setTitle(title)
-        markerOverlay.balloonView?.setSubTitle(subTitle)
-
-        return markerOverlay
-    }
-
-    override fun makeMarker(point: TMapPoint, markerBitmap: Bitmap): TMapMarkerItem {
-        val tMapMarkerItem: TMapMarkerItem = TMapMarkerItem()
-        tMapMarkerItem.tMapPoint = point
-        tMapMarkerItem.icon = markerBitmap
-        tMapMarkerItem.setPosition(0.5f, 1f)
-        tMapMarkerItem.name = "testMarker"
-        return tMapMarkerItem
-    }
-
-
-    override fun findMyLocation() {
-
-    }
-
-    override fun filterMap(category: CharCategory) {
-
+    companion object {
+        const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
