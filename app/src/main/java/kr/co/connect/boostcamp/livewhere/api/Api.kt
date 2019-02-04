6eb5658adff312e380fb70a1be280ff7ba894b64
최준영 <chad1994@skuniv.ac.kr>
2019-02-04 15:34:52 +0900

    #67 거주 경험 작성 api, repository 인터페이스 정의

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/api/Api.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/api/Api.kt
@@ -1,19 +1,26 @@
 package kr.co.connect.boostcamp.livewhere.api
 
+import android.provider.ContactsContract
 import io.reactivex.Single
 import kr.co.connect.boostcamp.livewhere.model.HouseResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
-interface Api{
+interface Api {
     @GET("house/search/info")
     fun getHouseDetail(
-        @Query("address")address:String
-    ):Single<Response<List<Any>>>
-    
+        @Query("address") address: String
+    ): Single<Response<List<Any>>>
+
     @GET("house/search/infos")
     fun getDetail(
-        @Query("address")address:String
+        @Query("address") address: String
     ): Single<HouseResponse>
+
+    @POST("")
+    fun postReview(
+        @Query("nickname") nickname: String, @Query("id") id: String, @Query("contents") contents: String
+    ) : Single<Response<Any>>
 }
