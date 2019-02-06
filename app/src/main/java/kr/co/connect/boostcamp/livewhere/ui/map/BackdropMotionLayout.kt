9dbc338a4baf228a92c6ae59cd625ae21fa59301
hakzzang <mbh930312@naver.com>
2019-02-06 21:50:56 +0900

    #78 백드롭 Layout View 구조 변경

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/map/BackdropMotionLayout.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/map/BackdropMotionLayout.kt
@@ -18,16 +18,11 @@ class BackdropMotionLayout (context: Context, attributeSet: AttributeSet? = null
 
     init {
         setTransitionListener(object : MotionLayout.TransitionListener {
-            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
+            override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {}
 
-            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
+            override fun onTransitionStarted(p0: MotionLayout?, p1: Int, p2: Int) {}
 
-            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {
-            }
+            override fun onTransitionChange(p0: MotionLayout?, p1: Int, p2: Int, p3: Float) {}
 
             override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
                 touchStarted = false
