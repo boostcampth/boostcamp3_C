0ab26b493dcc7d0fddc1b58eef7064aedb9907b8
최준영 <chad1994@skuniv.ac.kr>
2019-02-06 19:50:48 +0900

    #73 시세 추이 카테고리 상수 추가

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/util/ConstVal.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/util/ConstVal.kt
@@ -3,3 +3,5 @@ package kr.co.connect.boostcamp.livewhere.util
 const val charterAvg = "평균 전세가"
 const val No_barChart_text = "거래 기록이 없습니다"
 const val No_Review_Text = "해당 지역의 후기가 없습니다."
+const val TYPE_CHARTER = 1
+const val TYPE_MONTHLY = 2
