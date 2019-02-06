9749d12ed128330ed598c6f2b4adcced623b7938
hakzzang <mbh930312@naver.com>
2019-02-06 22:00:09 +0900

    #78 LiveApplication 구조 변경

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/LiveApplication.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/LiveApplication.kt
@@ -1,15 +1,13 @@
 package kr.co.connect.boostcamp.livewhere
 
 import android.app.Application
+import android.content.Context
 import com.bumptech.glide.Glide
-import com.crashlytics.android.Crashlytics
 import com.facebook.stetho.Stetho
 import com.naver.maps.map.NaverMapSdk
-import io.fabric.sdk.android.Fabric
 import kr.co.connect.boostcamp.livewhere.BuildConfig.NaverClientId
 import kr.co.connect.boostcamp.livewhere.di.appModules
 import org.koin.android.ext.android.startKoin
-
 
 class LiveApplication : Application() {
     override fun onCreate() {
@@ -17,19 +15,21 @@ class LiveApplication : Application() {
         Stetho.initializeWithDefaults(this)
         startKoin(applicationContext, appModules)
         //Firebase Crashlytics
-        Fabric.with(this, Crashlytics())
+        //Fabric.with(this, Crashlytics())
         NaverMapSdk.getInstance(this).client = NaverMapSdk.NaverCloudPlatformClient(NaverClientId)
+    }
+
+    override fun attachBaseContext(base: Context?) {
+        super.attachBaseContext(base)
     }
 
     override fun onLowMemory() {
         super.onLowMemory()
         Glide.get(this).clearMemory()
-        //GlideApp.get(this).clearMemory()
     }
 
     override fun onTrimMemory(level: Int) {
         super.onTrimMemory(level)
         Glide.get(this).trimMemory(level)
-        //GlideApp.get(this).trimMemory(level)
     }
 }
