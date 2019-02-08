f56bb0cda15c13ff3ac193034855995fd1eaef27
hakzzang <mbh930312@naver.com>
2019-02-08 10:29:45 +0900

    #75 application 클래스 리뷰관련 대응, multidex 및 Stetho

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/LiveApplication.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/LiveApplication.kt
@@ -2,6 +2,7 @@ package kr.co.connect.boostcamp.livewhere
 
 import android.app.Application
 import android.content.Context
+import androidx.multidex.MultiDex
 import com.bumptech.glide.Glide
 import com.facebook.stetho.Stetho
 import com.naver.maps.map.NaverMapSdk
@@ -12,15 +13,22 @@ import org.koin.android.ext.android.startKoin
 class LiveApplication : Application() {
     override fun onCreate() {
         super.onCreate()
-        Stetho.initializeWithDefaults(this)
+
+        //debug변수를 놓고 debug 인지 캐치함.
+        if(BuildConfig.isDebug){
+            Stetho.initializeWithDefaults(this)
+        }
+
         startKoin(applicationContext, appModules)
         //Firebase Crashlytics
         //Fabric.with(this, Crashlytics())
         NaverMapSdk.getInstance(this).client = NaverMapSdk.NaverCloudPlatformClient(NaverClientId)
     }
 
+    //application class에서 MultiDex 사용
     override fun attachBaseContext(base: Context?) {
         super.attachBaseContext(base)
+        MultiDex.install(this)
     }
 
     override fun onLowMemory() {
