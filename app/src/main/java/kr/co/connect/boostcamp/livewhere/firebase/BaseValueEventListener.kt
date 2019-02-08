4ecee1f2e1bb2017de109347a5a24303f15a9faa
최준영 <chad1994@skuniv.ac.kr>
2019-02-08 22:58:37 +0900

    #82 valueEventListener 구현

---
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/firebase/BaseValueEventListener.kt
@@ -0,0 +1,22 @@
+package kr.co.connect.boostcamp.livewhere.firebase
+
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+
+
+class BaseValueEventListener<Model, Entity>(
+    private val mapper: FirebaseMapper<Entity, Model>,
+    private val callback: FirebaseDatabaseRepository.FirebaseDatabaseRepositoryCallback<Model>,
+    private val pnu: String
+) : ValueEventListener {
+
+    override fun onDataChange(dataSnapshot: DataSnapshot) {
+        val data = mapper.mapList(pnu,dataSnapshot)
+        callback.onSuccess(data)
+    }
+
+    override fun onCancelled(databaseError: DatabaseError) {
+        callback.onError(databaseError.toException())
+    }
+}
