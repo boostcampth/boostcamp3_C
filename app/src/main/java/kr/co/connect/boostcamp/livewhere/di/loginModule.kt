a641406599504ec4650d9727632e4454f311180d
최준영 <chad1994@skuniv.ac.kr>
2019-02-11 12:18:29 +0900

    #108 login module 추가

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/di/loginModule.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/di/loginModule.kt
@@ -1,2 +1,15 @@
 package kr.co.connect.boostcamp.livewhere.di
 
+import kr.co.connect.boostcamp.livewhere.repository.LoginRepository
+import kr.co.connect.boostcamp.livewhere.repository.LoginRepositoryImpl
+import kr.co.connect.boostcamp.livewhere.ui.login.LoginViewModel
+import org.koin.dsl.module.module
+import org.koin.androidx.viewmodel.ext.koin.viewModel
+
+val loginModule = module {
+    factory {
+        LoginRepositoryImpl() as LoginRepository
+    }
+
+    viewModel { LoginViewModel(get()) }
+}
