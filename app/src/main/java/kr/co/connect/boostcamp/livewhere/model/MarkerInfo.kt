470bee35c42ccd506efee5587d79404271290cdc
hakzzang <mbh930312@naver.com>
2019-02-11 12:13:01 +0900

    #95 House, MarkerInfo data class에 parcelable 추가

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/model/MarkerInfo.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/model/MarkerInfo.kt
@@ -1,9 +1,12 @@
 package kr.co.connect.boostcamp.livewhere.model
 
+import android.os.Parcelable
 import com.naver.maps.geometry.LatLng
+import kotlinx.android.parcel.Parcelize
 import kr.co.connect.boostcamp.livewhere.util.StatusCode
 
+@Parcelize
 data class MarkerInfo(
     val latLng: LatLng,
     val houseList: List<House>,
-    val statusCode: StatusCode)
+    val statusCode: StatusCode) : Parcelable
