56bae2df02d94cd4de705208719ef957eaef7f9d
최준영 <chad1994@skuniv.ac.kr>
2019-02-11 12:19:20 +0900

    #108 login view 추가

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/login/LoginActivity.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/login/LoginActivity.kt
@@ -1,2 +1,44 @@
 package kr.co.connect.boostcamp.livewhere.ui.login
 
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import kr.co.connect.boostcamp.livewhere.R
+import kr.co.connect.boostcamp.livewhere.databinding.ActivityLoginBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
+
+class LoginActivity : AppCompatActivity() {
+
+    companion object {
+        private const val LOGIN_CONTAINER_ID = R.id.login_container
+    }
+
+    private val viewModel: LoginViewModel by viewModel()
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var currentFragment: Fragment
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, kr.co.connect.boostcamp.livewhere.R.layout.activity_detail)
+
+        if (savedInstanceState == null) {
+            addLoginFragment()
+        }
+
+        binding.apply {
+            viewModel = this@LoginActivity.viewModel
+            lifecycleOwner = this@LoginActivity
+        }
+
+    }
+
+    private fun addLoginFragment() {
+        val fragment = LoginFragment.newInstance()
+        currentFragment = fragment
+        supportFragmentManager
+            .beginTransaction()
+            .replace(LoginActivity.LOGIN_CONTAINER_ID, fragment)
+            .commit()
+    }
+}
--- app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/login/LoginFragment.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/login/LoginFragment.kt
@@ -1,2 +1,33 @@
 package kr.co.connect.boostcamp.livewhere.ui.login
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kr.co.connect.boostcamp.livewhere.databinding.FragmentLoginBinding
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+
+class LoginFragment: Fragment(){
+    companion object {
+        fun newInstance(): LoginFragment {
+            val args = Bundle()
+            val fragment = LoginFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+
+    private val viewModel: LoginViewModel by sharedViewModel()
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentLoginBinding.inflate(inflater,container,false).apply {
+            viewModel = this@LoginFragment.viewModel
+            lifecycleOwner = this@LoginFragment
+        }
+
+        return binding.root
+    }
+
+}
