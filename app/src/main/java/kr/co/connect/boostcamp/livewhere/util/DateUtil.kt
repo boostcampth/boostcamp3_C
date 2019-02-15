19cc98e143936211f6d7c1d082212519a29e6c2b
최준영 <chad1994@skuniv.ac.kr>
2019-02-15 21:35:13 +0900

    #156 현재날짜 구하는 util추가

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/util/DateUtil.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/util/DateUtil.kt
@@ -1,2 +1,16 @@
 package kr.co.connect.boostcamp.livewhere.util
 
+import android.annotation.SuppressLint
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+object DateUtil {
+    @SuppressLint("SimpleDateFormat")
+    fun getCurrentDate() : String? {
+        val now = System.currentTimeMillis()
+        val date = Date(now)
+        val sdf = SimpleDateFormat("yyyy년 MM월 dd일")
+        return sdf.format(date)
+    }
+}
