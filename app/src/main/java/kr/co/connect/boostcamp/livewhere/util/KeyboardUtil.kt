06b630e2a659abec5de60a4083576c6fedd435fd
최준영 <chad1994@skuniv.ac.kr>
2019-02-16 23:17:45 +0900

    #166 키보드 show/hide util추가

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/util/KeyboardUtil.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/util/KeyboardUtil.kt
@@ -1,2 +1,14 @@
 package kr.co.connect.boostcamp.livewhere.util
 
+import android.app.Activity
+import android.view.inputmethod.InputMethodManager
+
+fun Activity.keyboardShow(){
+    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0)
+}
+
+fun Activity.keyboardHide(){
+    val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    inputMethodManager.hideSoftInputFromWindow(currentFocus?.windowToken, 0)
+}
