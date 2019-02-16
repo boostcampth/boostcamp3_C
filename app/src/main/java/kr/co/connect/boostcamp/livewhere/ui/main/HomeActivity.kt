f146f05e0e5f75c0ea269ec0917bf17ff5537277
weedrice <supp0rtyoo@gmail.com>
2019-02-17 03:31:15 +0900

    #155 Add Animaion, Delete SearchViewModel in HomeActivity

--- app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/main/HomeActivity.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/main/HomeActivity.kt
@@ -1,9 +1,7 @@
 package kr.co.connect.boostcamp.livewhere.ui.main
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -11,15 +9,11 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.net.PlacesClient
-import io.fabric.sdk.android.services.common.CommonUtils.hideKeyboard
-import kotlinx.android.synthetic.main.fragment_search.*
 import kr.co.connect.boostcamp.livewhere.BuildConfig
 import kr.co.connect.boostcamp.livewhere.R
 import kr.co.connect.boostcamp.livewhere.databinding.ActivityHomeBinding
 import kr.co.connect.boostcamp.livewhere.ui.map.MapActivity
 import kr.co.connect.boostcamp.livewhere.util.APPLICATION_EXIT
-import kr.co.connect.boostcamp.livewhere.util.DELETE_RECENT_SEARCH
-import kr.co.connect.boostcamp.livewhere.util.EMPTY_STRING_TEXT
 import kr.co.connect.boostcamp.livewhere.util.SEARCH_TAG
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -30,9 +24,8 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private lateinit var binding: ActivityHomeBinding
-    private lateinit var placesClient: PlacesClient
+    lateinit var placesClient: PlacesClient
     private val homeViewModel: HomeViewModel by viewModel()
-    private val searchViewModel: SearchViewModel by viewModel()
     private lateinit var currentFragment: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,17 +41,11 @@ class HomeActivity : AppCompatActivity() {
 
         binding.apply {
             homeViewModel = this@HomeActivity.homeViewModel
-            searchViewModel = this@HomeActivity.searchViewModel
             lifecycleOwner = this@HomeActivity
         }
 
-        setGoogleClient()
         observeValues()
         initBookmark()
-    }
-
-    private fun setGoogleClient() {
-        searchViewModel.setClient(placesClient)
     }
 
     private fun initBookmark() {
@@ -73,48 +60,18 @@ class HomeActivity : AppCompatActivity() {
         homeViewModel.sendAddress.observe(this, Observer {
             startMapActivity(homeViewModel.sendAddress.value)
         })
-
-        searchViewModel.backBtnClicked.observe(this, Observer {
-            hideKeyboard()
-            startHomeFragment()
-        })
-
-        searchViewModel.mapBtnClicked.observe(this, Observer {
-            startMapActivity()
-        })
-
-        searchViewModel.searchText.observe(this, Observer {
-            if(searchViewModel.searchText.value.isNullOrEmpty()) {
-                Toast.makeText(this, EMPTY_STRING_TEXT, Toast.LENGTH_LONG).show()
-            } else {
-                if(currentFragment.et_search_bar.text.toString() == searchViewModel.searchText.toString()) {
-                    if(searchViewModel.autoCompleteList.value.isNullOrEmpty()) {
-                        startMapActivity(searchViewModel.autoCompleteList.value!![0])
-                    }
-                } else {
-                    startMapActivity(searchViewModel.searchText.value)
-                }
-            }
-        })
-
-        searchViewModel.showToast.observe(this, Observer {
-            if(it) {
-                Toast.makeText(this, DELETE_RECENT_SEARCH, Toast.LENGTH_LONG).show()
-                searchViewModel.setToastDone()
-            }
-        })
     }
 
-    private fun hideKeyboard() {
-        currentFragment.et_search_bar.inputType = 0
-        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(currentFragment.et_search_bar.windowToken, 0)
-    }
-
-    private fun startHomeFragment(){
+    fun startHomeFragment(){
         currentFragment = HomeFragment.newInstance()
         supportFragmentManager
             .beginTransaction()
+            .setCustomAnimations(
+                R.anim.slide_in_right,
+                R.anim.slide_out_right,
+                R.anim.slide_in_left,
+                R.anim.slide_out_left
+            )
             .replace(HOME_CONTAINER_ID, currentFragment)
             .commit()
     }
@@ -123,17 +80,23 @@ class HomeActivity : AppCompatActivity() {
         currentFragment = SearchFragment.newInstance()
         supportFragmentManager
             .beginTransaction()
+            .setCustomAnimations(
+                R.anim.slide_in_left,
+                R.anim.slide_out_left,
+                R.anim.slide_in_right,
+                R.anim.slide_out_right
+            )
             .replace(HOME_CONTAINER_ID, currentFragment)
+            .addToBackStack(null)
             .commit()
-        searchViewModel.setVisibility()
     }
 
-    private fun startMapActivity() {
+    fun startMapActivity() {
         intent = Intent(this, MapActivity::class.java)
         startActivity(intent)
     }
 
-    private fun startMapActivity(text: String?) {
+    fun startMapActivity(text: String?) {
         if(text != null) {
             intent = Intent(this, MapActivity::class.java)
             intent.putExtra(SEARCH_TAG, text)
--- app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/main/SearchFragment.kt
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/ui/main/SearchFragment.kt
@@ -1,23 +1,30 @@
 package kr.co.connect.boostcamp.livewhere.ui.main
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.libraries.places.api.net.PlacesClient
+import io.fabric.sdk.android.services.common.CommonUtils.hideKeyboard
 import kotlinx.android.synthetic.main.fragment_recent_search.*
 import kotlinx.android.synthetic.main.fragment_recent_search.view.*
+import kotlinx.android.synthetic.main.fragment_search.*
+import kotlinx.android.synthetic.main.fragment_search.view.*
 import kr.co.connect.boostcamp.livewhere.databinding.FragmentSearchBinding
 import kr.co.connect.boostcamp.livewhere.ui.main.adapter.AutoCompleteRecyclerViewAdapter
 import kr.co.connect.boostcamp.livewhere.ui.main.adapter.RecentSearchRecyclerViewAdapter
 import kr.co.connect.boostcamp.livewhere.util.DELETE_RECENT_SEARCH
+import kr.co.connect.boostcamp.livewhere.util.EMPTY_STRING_TEXT
 import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class SearchFragment : Fragment() {
-
     companion object {
         fun newInstance(): SearchFragment {
             val args = Bundle()
@@ -72,7 +79,58 @@ class SearchFragment : Fragment() {
             adapter = autoCompleteRecyclerViewAdapter
         }
 
+        setClient()
+
         return binding.root
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        searchViewModel.setVisibility()
+
+        startObserve()
+    }
+
+    private fun startObserve() {
+        searchViewModel.backBtnClicked.observe(this, Observer {
+            hideKeyboard()
+            (activity as HomeActivity).startHomeFragment()
+        })
+
+        searchViewModel.mapBtnClicked.observe(this, Observer {
+            (activity as HomeActivity).startMapActivity()
+        })
+
+        searchViewModel.searchText.observe(this, Observer {
+            if(searchViewModel.searchText.value.isNullOrEmpty()) {
+                Toast.makeText(activity as HomeActivity, EMPTY_STRING_TEXT, Toast.LENGTH_LONG).show()
+            } else {
+                if(binding.etSearchBar.text.toString() == searchViewModel.searchText.toString()) {
+                    if(searchViewModel.autoCompleteList.value.isNullOrEmpty()) {
+                        (activity as HomeActivity).startMapActivity(searchViewModel.autoCompleteList.value!![0])
+                    }
+                } else {
+                    (activity as HomeActivity).startMapActivity(searchViewModel.searchText.value)
+                }
+            }
+        })
+
+        searchViewModel.showToast.observe(this, Observer {
+            if(it) {
+                Toast.makeText(activity as HomeActivity, DELETE_RECENT_SEARCH, Toast.LENGTH_LONG).show()
+                searchViewModel.setToastDone()
+            }
+        })
+    }
+
+    private fun hideKeyboard() {
+        binding.etSearchBar.inputType = 0
+        val imm = (activity as HomeActivity).getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(binding.etSearchBar.windowToken, 0)
+    }
+
+    private fun setClient() {
+        searchViewModel.setClient((activity as HomeActivity).placesClient)
     }
 
     private fun changeSearchRv(set: Boolean) {
