c1621bf321442d78b62544e8a309252ea8537d53
최준영 <chad1994@skuniv.ac.kr>
2019-02-20 13:35:42 +0900

    #208 스트리트뷰 실행을 위한 값 모델

---
+++ app/src/main/java/kr/co/connect/boostcamp/livewhere/model/StreetView.kt
@@ -0,0 +1,7 @@
+package kr.co.connect.boostcamp.livewhere.model
+
+data class StreetView(
+    var latitude: String? = null,
+    var longitude: String? = null,
+    var addressName: String? = null
+)
